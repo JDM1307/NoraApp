0049c13194fd50dfb0c31fc67a5dc4e815c5d85a
JDM <davill130101@gmail.com>
2025-02-17 14:07:27 -0500

    Actualizar .gitignore

--- Nora/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class NoraConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Nora'
--- Nora/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class NoraTests(TestCase):
-    def test_example(self):
-        self.assertEqual(1 + 1, 2)  # Example test case to ensure the test framework is working.
